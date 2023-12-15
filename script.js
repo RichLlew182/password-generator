98eb5e69866dffc9656284b9b7863543edd1dbd6
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2023-12-15 18:49:42 +0000

    updated pseudo code to include TODO

--- script.js
+++ script.js
@@ -12,39 +12,39 @@ var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K
 
 const charOptions = [];
 const generatedPassword = '';
-// You can store the generatedPassword as a string and concat each character OR
-// as an array and push each character, then join once you have enough characters
+// TODO You can store the generatedPassword as a string and concat each character OR
+// TODO as an array and push each character, then join once you have enough characters
 
 // Function to prompt user for password options
 function getPasswordOptions() {
 
-  // Prompt for password length
-  // At least 8 characters, no more than 128 characters
-  // Conditional to check that the number that was entered is in range
-  // Prompts store data as strings, so need to parse into a number
-  // If the user's input is out of range, either return out of the function or call the function again
+  // TODO Prompt for password length
+  // TODO At least 8 characters, no more than 128 characters
+  // TODO Conditional to check that the number that was entered is in range
+  // TODO Prompts store data as strings, so need to parse into a number
+  // TODO If the user's input is out of range, either return out of the function or call the function again
 
-  // Once they select a character set:
-  // Generate a random character for each selected character set
-  // Either push selected character sets to a mega-array of all selected characters
-  // OR you can keep the arrays separate and generate a random number to select the array and another to select the index
+  // TODO Once they select a character set:
+  // TODO Generate a random character for each selected character set
+  // TODO Either push selected character sets to a mega-array of all selected characters
+  // TODO OR you can keep the arrays separate and generate a random number to select the array and another to select the index
 
-  // Once character sets are selected, move on to generating random characters
+  // TODO Once character sets are selected, move on to generating random characters
 
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
 
-  // Need a variable to hold the password as it's being generated
-  // Need a variable to hold the index that's being generated
+  // TODO Need a variable to hold the password as it's being generated
+  // TODO Need a variable to hold the index that's being generated
 
-  // For loop that loops the number of times that matches the length the user chose
-  // Generate a random number
-  // That number is the index for a character in the mega-array
-  // So then, mega-array[generated-index] is the actual character
-  // Add that character to the password
-  // Once we finish the for loop, return the generated password
+  // TODO For loop that loops the number of times that matches the length the user chose
+  // TODO Generate a random number
+  // TODO That number is the index for a character in the mega-array
+  // TODO So then, mega-array[generated-index] is the actual character
+  // TODO Add that character to the password
+  // TODO Once we finish the for loop, return the generated password
 
 }
 
