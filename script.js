6d70b2cdc1b8e66840b5365e7bb522e858acd35d
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2023-12-18 12:15:08 +0000

    cleaned up some comments. The script is nearly there but just needs a few more conditionals to stop any errors

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+// You can store the generatedPassword as a string and concat each character OR
+// as an array and push each character, then join once you have enough characters
 var charOptions = [];
 var generatedPassword = '';
-// TODO You can store the generatedPassword as a string and concat each character OR
-// TODO as an array and push each character, then join once you have enough characters
 
 var userPasswordLengthGlobal;
 
@@ -23,8 +23,12 @@ function getPasswordOptions() {
   // Prompt for password length
 
   var userPasswordLengthLocal = prompt(`Let's Generate a password. Please input a number between 8 and 128.`);
-  userPasswordLengthGlobal = userPasswordLengthLocal;
-  console.log(`The password length is ${userPasswordLengthGlobal}`)
+  console.log(typeof userPasswordLengthLocal);
+
+  if (userPasswordLengthLocal === '') {
+    alert(`You didn't type anything!`);
+    getPasswordOptions()
+  }
 
   // alert(typeof userPasswordLength);
 
@@ -32,18 +36,21 @@ function getPasswordOptions() {
     return /^[0-9]*$/.test(userPasswordLengthLocal);
   }
 
-  console.log(typedNumbers(userPasswordLengthLocal));
-
   if (!typedNumbers(userPasswordLengthLocal)) {
     alert(`You didn't type a number!`)
     getPasswordOptions()
   }
 
+  console.log(`The password length is ${userPasswordLengthLocal}`)
+  console.log(typedNumbers(userPasswordLengthLocal));
+
   // Prompts store data as strings, so need to parse into a number
 
-  userPasswordLengthLocal = parseInt(userPasswordLengthLocal)
+  userPasswordLengthLocal = parseInt(userPasswordLengthLocal);
   console.log(userPasswordLengthLocal)
+  userPasswordLengthGlobal = userPasswordLengthLocal;
   // alert(typeof userPasswordLength);
+
 
   // At least 8 characters, no more than 128 characters
   // Conditional to check that the number that was entered is in range
@@ -79,9 +86,8 @@ For example @ % + # $ etc`);
 
   var confirmUpperCase = confirm(`Do you want to use Upper Case Characters in your Password?`)
 
-
-  // TODO Once they select a character set:
-  // TODO Generate a random character for each selected character set and add this to the generated password
+  //  Once they select a character set:
+  //  Generate a random character for each selected character set and add this to the generated password
 
   randomSpecCharInt = Math.floor(Math.random() * specialCharacters.length);
   console.log(randomSpecCharInt);
@@ -103,8 +109,10 @@ For example @ % + # $ etc`);
   randomUpperCaseChar = upperCasedCharacters[randomUpperCaseCharInt];
   console.log(randomUpperCaseChar);
 
+  // TODO need an else statement for if no characters are selected and if so start the function over
 
-  // TODO Then Either push selected character sets to a mega-array of all selected characters
+
+  //  Then Either push selected character sets to a mega-array of all selected characters
 
   if (confirmSpecChars) {
     generatedPassword += randomSpecChar;
@@ -154,63 +162,50 @@ For example @ % + # $ etc`);
     charOptions = charOptions;
   }
 
-
-  // TODO OR you can keep the arrays separate and generate a random number to select the array and another to select the index
-
-  // TODO Once character sets are selected, move on to generating random characters
-
-
-
 }
 
+var randomMegaIntGlobal;
 
 // Function for getting a random element from an array
 function getRandom(arr) {
 
-  // TODO Need a variable to hold the password as it's being generated
-
-
-  // TODO Need a variable to hold the index that's being generated
-  // TODO Generate a random number
+  //  Need a variable to hold the index that's being generated
+  //  Generate a random number
 
   var randomMegaIntLocal = Math.floor(Math.random() * arr.length);
-  // console.log(randomMegaIntLocal);
 
-  // TODO For loop that loops the number of times that matches the length the user chose
-
-  // for (i = 0; i < userPasswordLengthGlobal; i++) {
-  //   console.log(arr[randomMegaIntLocal])
-  // }
-
-
-  // TODO That number is the index for a character in the mega-array
-  // TODO So then, mega-array[generated-index] is the actual character
-  // TODO Add that character to the password
-  // TODO Once we finish the for loop, return the generated password
 
   randomMegaIntGlobal = randomMegaIntLocal;
-  return randomMegaIntLocal
+  return;
 
 }
-
-// getRandom(charOptions);
-// console.log(randomMegaIntGlobal);
-// console.log(`The password so far is ${generatedPassword}`);
 
 // Function to generate password with user input
 function generatePassword() {
 
-  getPasswordOptions()
+  getPasswordOptions();
+
+  // For loop that loops the number of times that matches the length the user chose
 
   for (let i = 0; i < userPasswordLengthGlobal; i++) {
     console.log(`The User password length is ${userPasswordLengthGlobal}`)
+
     getRandom(charOptions);
+
     console.log(`The Get Random result is ${getRandom(charOptions)}`)
+
+    // Add that character to the password
+    // That number is the index for a character in the mega-array
+    // So then, mega-array[generated-index] is the actual character
+
     generatedPassword += charOptions[randomMegaIntGlobal];
     console.log(`The password length is ${generatedPassword.length}`)
   }
   console.log(`The generated password is ${generatedPassword}`);
   console.log(`The generated password length is ${generatedPassword.length} characters`);
+
+  // Once we finish the for loop, return the generated password
+
   return generatedPassword;
 
 }
